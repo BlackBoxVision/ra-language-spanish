4afe9d76d03b8d8c404c4b7a38f245350116ccad
jonatansalas <jonatansalas@live.com.ar>
2017-03-07 17:07:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Spanish Translations for Admin-on-rest
+
+Spanish translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+## Installation
+
+```sh
+npm install --save aor-language-spanish
+```
+
+## Usage
+
+```js
+import spanishMessages from 'aor-language-spanish';
+
+const messages = {
+    'es': spanishMessages,
+};
+
+<Admin locale="es" messages={messages}>
+  ...
+</Admin>
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE), and sponsored by [BlackBox Vision](https://github.com/BlackBoxVision).
---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+    aor: {
+        action: {
+            delete: 'Eliminar',
+            show: 'Mostrar',
+            list: 'Listar',
+            save: 'Guardar',
+            create: 'Crear',
+            edit: 'Editar',
+            cancel: 'Cancelar',
+            refresh: 'Actualizar',
+            add_filter: 'Añadir filtro',
+            remove_filter: 'Eliminar filtro',
+        },
+        boolean: {
+            true: 'Si',
+            false: 'No',
+        },
+        page: {
+            list: 'Listar %{name}',
+            edit: '%{name} #%{id}',
+            show: '%{name} #%{id}',
+            create: 'Crear %{name}',
+            delete: 'Eliminar %{name} #%{id}',
+        },
+        input: {
+            image: {
+                upload_several: 'Suelte algunos archivos para cargar, o haga clic para seleccionar uno.',
+                upload_single: 'Suelte un archivo para cargarlo o haga clic para seleccionarlo.',
+            },
+        },
+        message: {
+            yes: 'Si',
+            no: 'No',
+            are_you_sure: 'Estás seguro?',
+            about: 'Acerca de',
+        },
+        navigation: {
+            page_out_of_boundaries: 'La página %{page} esta fuera del limite',
+            page_out_from_end: 'No se puede ir después de la última página',
+            page_out_from_begin: 'No se puede ir antes de la página 1',
+            page_range_info: '%{offsetBegin}-%{offsetEnd} de %{total}',
+            next: 'Siguiente',
+            prev: 'Anterior',
+        },
+        auth: {
+            username: 'Usuario',
+            password: 'Contraseña',
+            sign_in: 'Ingresar',
+            sign_in_error: 'Se ha producido un error en la autenticación.',
+            logout: 'Cerrar sesión',
+        },
+        notification: {
+            updated: 'Elemento actualizado',
+            created: 'Elemento creado',
+            deleted: 'Elemento eliminado',
+            item_doesnt_exist: 'El elemento no existe',
+        },
+        validation: {
+            required: 'Requerido',
+        },
+    },
+};
