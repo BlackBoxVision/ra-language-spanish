bc465843bc91f03f55579d6abd5e963dec6faa04
Ignacio Lago <ignacio@ignaciolago.com>
2018-02-27 11:03:39 +0100

    Add "aor.action.back"
    Instead of "Ir atrás", I've used "Atrás" to adapt the length to the usual usages of the string.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
             refresh: 'Actualizar',
             add_filter: 'Añadir filtro',
             remove_filter: 'Eliminar filtro',
+            back: 'Atrás',
         },
         boolean: {
             true: 'Sí',
