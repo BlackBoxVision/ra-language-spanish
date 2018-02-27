816d5605737e931f182e94e0135b8d5151c20231
Ignacio Lago <ignacio@ignaciolago.com>
2018-02-27 15:01:50 +0100

    Add "not_found" to pages and messages

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ module.exports = {
             show: '%{name} #%{id}',
             create: 'Crear %{name}',
             delete: 'Eliminar %{name} #%{id}',
-            dashboard: 'Panel de control'
+            dashboard: 'Panel de control',
+            not_found: 'No encontrada',
         },
         input: {
             image: {
@@ -36,6 +37,7 @@ module.exports = {
             no: 'No',
             are_you_sure: '¿Estás seguro?',
             about: 'Acerca de',
+            not_found: 'Puede que escribieses una URL equivocada, o que siguieses un enlace erróneo.',
         },
         navigation: {
             no_results: 'No se encontraron resultados',
