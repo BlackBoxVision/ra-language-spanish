d1715b0d5ead4327eaa3f5e790976b789301c9a3
Jonatan E. Salas <jonatansalas@live.com.ar>
2018-05-27 21:43:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,79 @@
 module.exports = {
-    aor: {
+    ra: {
         action: {
             delete: 'Eliminar',
             show: 'Mostrar',
             list: 'Listar',
             save: 'Guardar',
-            cancel: 'Cancelar',
             create: 'Crear',
             edit: 'Editar',
-            refresh: 'Actualizar',
+            sort: 'Ordenar',
+            cancel: 'Cancelar',
+            undo: 'Deshacer',
+            refresh: 'Refrescar',
+            add: 'Añadir',
+            remove: 'Remover',
             add_filter: 'Añadir filtro',
-            remove_filter: 'Eliminar filtro',
-            back: 'Atrás',
+            remove_filter: 'Remover este filtro',
+            back: 'Ir Atrás',
+            bulk_actions: '%{smart_count} seleccionados',
         },
         boolean: {
-            true: 'Sí',
+            true: 'Si',
             false: 'No',
         },
         page: {
-            list: 'Listar %{name}',
+            list: '%{name} Lista',
             edit: '%{name} #%{id}',
             show: '%{name} #%{id}',
             create: 'Crear %{name}',
-            delete: 'Eliminar %{name} #%{id}',
-            dashboard: 'Panel de control',
-            not_found: 'No encontrada',
+            dashboard: 'Dashboard',
+            not_found: 'No encontrado',
+            loading: 'Cargando',
         },
         input: {
+            file: {
+                upload_several:
+                    'Arroja algunos archivos para subir, o clickea para seleccionar uno.',
+                upload_single: 'Arroja un archivo para subir, o clickea para seleccionar uno.',
+            },
             image: {
-                upload_several: 'Suelte algunos archivos para cargar o haga clic para seleccionar uno.',
-                upload_single: 'Suelte un archivo para cargarlo o haga clic para seleccionarlo.',
+                upload_several:
+                    'Arroja algunos imagénes para subir, o clickea para seleccionar una.',
+                upload_single:
+                    'Arroja algunos imagen para subir, o clickea para seleccionar una.',
+            },
+            references: {
+                all_missing: 'No se pueden encontrar datos de referencias.',
+                many_missing:
+                    'Al menos una de las referencias asociadas ya no parece estar disponible.',
+                single_missing:
+                    'La referencia asociada ya no parece estar disponible.',
             },
         },
         message: {
-            yes: 'Sí',
+            yes: 'Si',
             no: 'No',
             are_you_sure: '¿Estás seguro?',
             about: 'Acerca de',
-            not_found: 'Puede que escribieses una URL equivocada, o que siguieses un enlace erróneo.',
+            not_found:
+                'O bien escribió una URL incorrecta o siguió un enlace incorrecto.',
+            loading: 'La página se está cargando, espere un momento por favor',
+            invalid_form: 'El formulario no es válido. Por favor verifique si hay errores',
+            delete_title: 'Eliminar %{name} #%{id}',
+            delete_content: '¿Seguro que quieres eliminar este artículo?',
+            bulk_delete_title:
+                'Eliminar %{name} |||| Eliminar %{smart_count} %{name} articulos',
+            bulk_delete_content:
+                '¿Seguro que quieres eliminar este %{name}? |||| ¿Seguro que quieres eliminar estos %{smart_count} articulos?',
         },
         navigation: {
-            no_results: 'No se encontraron resultados',
-            page_out_of_boundaries: 'La página %{page} está fuera del limite',
-            page_out_from_end: 'No se puede ir después de la última página',
-            page_out_from_begin: 'No se puede ir antes de la página 1',
+            no_results: 'No se han encontrado resultados',
+            no_more_results:
+                'El número de página %{page} está fuera de los límites. Prueba la página anterior.',
+            page_out_of_boundaries: 'Número de página %{page} fuera de los límites',
+            page_out_from_end: 'No puede ir después de la última página',
+            page_out_from_begin: 'No puede ir antes de la página 1',
             page_range_info: '%{offsetBegin}-%{offsetEnd} de %{total}',
             next: 'Siguiente',
             prev: 'Anterior',
@@ -51,18 +81,29 @@ module.exports = {
         auth: {
             username: 'Usuario',
             password: 'Contraseña',
-            sign_in: 'Acceder',
-            sign_in_error: 'Se ha producido un error en la autenticación.',
-            logout: 'Cerrar sesión',
+            sign_in: 'Registrarse',
+            sign_in_error: 'La autenticación falló, por favor, vuelva a intentarlo',
+            logout: 'Cerrar Sesión',
         },
         notification: {
-            updated: 'Elemento actualizado',
+            updated: 'Elemento actualizado |||| %{smart_count} elementos actualizados',
             created: 'Elemento creado',
-            deleted: 'Elemento eliminado',
+            deleted: 'Elemento borrado |||| %{smart_count} elementos borrados.',
+            bad_item: 'El elemento es incorrecto',
             item_doesnt_exist: 'El elemento no existe',
+            http_error: 'Error de comunicación del servidor',
+            canceled: 'Acción cancelada',
         },
         validation: {
             required: 'Requerido',
+            minLength: 'Debe contener %{min} caracteres al menos',
+            maxLength: 'Debe contener %{max} caracteres o menos',
+            minValue: 'Debe ser al menos %{min}',
+            maxValue: 'Debe ser %{max} o menos',
+            number: 'Debe que ser un número',
+            email: 'Debe ser un correo electrónico válido',
+            oneOf: 'Debe ser uno de: %{options}',
+            regex: 'Debe coincidir con un formato específico (regexp): %{pattern}',
         },
     },
 };
